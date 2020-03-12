ca8920450621e6e3859b4ae6c56c2fd6166ff4e1
amineneifer <amineneifer2000@gmail.com>
2020-03-12 19:50:20 +0000

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - size of a tree.
+ *
+ * @tree: root.
+ *
+ * Return: size.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	else
+		return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
+}
