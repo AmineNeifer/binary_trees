f7fcfbffd2352dc1cac9d8abe8d216909ba1e083
amineneifer <amineneifer2000@gmail.com>
2020-03-12 20:39:12 +0000

    my way of calculating the height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0;
 	size_t left_height = 0;
 
-	if (!tree)
+	if (!tree || (!tree->right && !tree->left))
 		return (0);
 
-	if (tree->left || tree->right)
-	{
-		right_height = binary_tree_height(tree->right) + 1;
-		left_height = binary_tree_height(tree->left) + 1;
-	}
+	right_height = binary_tree_height(tree->right) + 1;
+	left_height = binary_tree_height(tree->left) + 1;
 
 	if (left_height > right_height)
 		return (left_height);
