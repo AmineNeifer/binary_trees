eb78de645b375ac7b61cfe9de8696ebb88a66633
amineneifer <amineneifer2000@gmail.com>
2020-03-12 13:24:47 +0000

    task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_preorder - preorder traversing
+ *
+ * @tree: pointer to the root.
+ * @func: pointer to a printing function.
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!func || !tree)
