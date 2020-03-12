7eeb60a61b169d0d1992004953faf24f81339f55
amineneifer <amineneifer2000@gmail.com>
2020-03-12 17:59:37 +0000

    ya alah

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * max - return the bigger number.
+ *
+ * @one: first value.
+ * @two: second value.
+ *
+ * Return: the bigger number.
+ */
+int max(int one, int two)
+{
+	if (one > two)
+		return (one);
+	return (two);
+}
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int right_height;
+	int left_height;
+
+	if (!tree)
+		return (-1);
+
+	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	return (max(right_height, left_height) + 1);
+}
