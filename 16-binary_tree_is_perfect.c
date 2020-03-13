75aa873e4fcd3ecb56406baa780cafb4f088dbcd
amineneifer <amineneifer2000@gmail.com>
2020-03-13 20:19:49 +0000

    changed order of functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -68,7 +68,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!balanced(tree) && full(tree))
+	if (full(tree) && !balanced(tree))
 	{
 		binary_tree_is_perfect(tree->right);
 		binary_tree_is_perfect(tree->left);
